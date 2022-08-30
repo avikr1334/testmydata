8cf0195f898e20b4d8c7181e7d99560a831c9a1b
SimpliLearn Github <avik.roy.uemk.cse.2023@gmail.com>
2022-08-30 22:22:55 +0530

    card bootstrap

--- index.js
+++ index.js
@@ -4,7 +4,13 @@ const app = express()
 app.set('view engine', 'ejs');
 
 app.get('/',(req,res)=>{
-    res.render('index')
+    const data = {
+        name:"subhankar dutta",
+        college:"UEMK",
+        image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSiY_BBq9EJopJ7s6xGctOMFLvDhY7LPCIesM18ezaj&s",
+        email:"subhankar0810@gmail.com"
+    }
+    res.render('index',data)
 })
 app.listen(process.env.PORT || 3000,()=>{
     console.log("connexted to 127.0.0.1:80");
