e16d61fef15492e73130214a7a0c4219c17b6ec4
SimpliLearn Github <avik.roy.uemk.cse.2023@gmail.com>
2022-08-30 22:27:53 +0530

    card bootstrap

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.get('/',(req,res)=>{
         image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSiY_BBq9EJopJ7s6xGctOMFLvDhY7LPCIesM18ezaj&s",
         email:"subhankar0810@gmail.com"
     }
-    res.render('index',data)
+    res.render('index',{data:data})
 })
 app.listen(process.env.PORT || 3000,()=>{
     console.log("connexted to 127.0.0.1:80");
